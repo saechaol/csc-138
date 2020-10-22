5350d3a93da57546194ade5f2ed204e30c47822d
Lucas <33850575+saechaol@users.noreply.github.com>
2020-10-22 04:19:48 -0700

    Implemented test udp client and server

---
+++ src/ToyUDPClient.py
@@ -0,0 +1,23 @@
+from socket import * # includes python's socket library
+
+serverName = 'hostname'
+serverPort = 12000
+
+# create UDP socket for server
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+# get user input from screen
+message = raw_input('Input lowercase sentence:')
+
+# attach server name, port to message, and send into socket
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+# retrieves the modified reply message from socket into a string
+# returns the message, and the ip address of the message
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+# prints message to the console
+print modifiedMessage.decode()
+
+# close the socket
+clientSocket.close()
---
+++ src/ToyUDPServer.py
@@ -0,0 +1,17 @@
+# import socket library
+from socket import *
+
+# specify port
+serverPort = 12000
+
+# create a UDP socket 
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# bind the socket to port 12000
+serverSocket.bind(('', serverPort))
+
+print("The server is ready to receive")
+while True:
+	message, clientAddress = serverSocket.recvfrom(2048)
+	modifiedMessage = message.decode().upper()
+	serverSocket.sendTo(modifiedMessage.encode(), clientAddress)
